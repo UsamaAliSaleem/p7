1b17a91cad7471aa6788e0174007f8c1843ebb0d
Usama Ali Saleem <usamaalisaleem816@gmail.com>
2020-08-06 08:45:16 +0500

    Revert "ok"
    This reverts commit edf9f4437e42946d7208eec42f6a3f98dd8f6758.

--- src/swDev.js
+++
@@ -1,7 +0,0 @@
-export default function swDev(){
-let swurl=`${process.env.PUBLIC_URL}/sw.js`
-navigator.serviceWorker.register(swurl).then((Response)=>{
-    console.warn("response",Response)
-})
-
-}
